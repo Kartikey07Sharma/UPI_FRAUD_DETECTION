699acced31025647e385c3ec31ca42c54cc967a1
Kartikey Sharma <kartikeysharma2004@gmail.com>
2026-03-07 12:53:53 +0530

    Delete ml_pipeline directory

--- ml_pipeline/db_test.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-def get_test_connection():
-    return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="Kartikey@123",
-        database="upi_fraud_test_db"
-    )
